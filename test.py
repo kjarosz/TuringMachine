52fd23a2aa77a5ae54a305e27fbf9caa714f8b83
Kamil Jarosz <kjarosz19946@gmail.com>
2015-12-27 18:46:17 -0500

    Added matcher to test script.

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 from turing_machine import TuringMachine
 
+print "Testing converter (abbaaababaaabaa -> bbbbbbbbbbbbbbb)" 
 tm = TuringMachine("converter.desc")
 tm.set_starting_tape("abbaaababaaabaa")
 tm.reset()
 tm.run(True)
 
+print ""
+print "Testing matcher (Expecting acceptance)"
+tm = TuringMachine("matcher.desc")
+tm.set_starting_tape("BaaaabbbbccccB")
+tm.reset()
+tm.run(True)
+
+print ""
+print "Testing matcher (Expecting rejection)"
+tm.set_starting_tape("BaaaabbbbcccB")
+tm.reset()
+tm.run(True)
